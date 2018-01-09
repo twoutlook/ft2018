2adaa6e7c619a0cc8f9e9c9561ee7ce9e562cdd6
Mark <twcloudwebapp@outlook.com>
2018-01-09 15:36:45 +0800

    up

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -9,7 +9,7 @@ from django.contrib import admin
 from .models import Choice, Question
 
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
