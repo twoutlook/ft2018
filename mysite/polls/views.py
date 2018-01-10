ae5faedfbd504bf369e23617e0e53664272ecfbb
Mark <twcloudwebapp@outlook.com>
2018-01-10 11:13:23 +0800

    fix view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -23,7 +23,7 @@ from .models import Choice, Question
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    latest_question_list = Question.objects.order_by('-pub_date')[:12]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
