8f698509ae499e0e26556ec577fac2446d079b6d
Mark <twcloudwebapp@outlook.com>
2018-01-10 16:58:08 +0800

    put polls to root

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -2,8 +2,9 @@ from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
+    # path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('', include('polls.urls')),
 ]
 # from django.contrib import admin
 # from django.urls import path
